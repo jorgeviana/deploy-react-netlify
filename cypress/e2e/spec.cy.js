f2068b7027501fc04c9103c65dc6666e363c9c19
Jorge Viana <14880391+jorgeviana@users.noreply.github.com>
2024-07-29 17:53:17 +0200

    assert the right value in e2e test: rev 2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://my-fe-app.netlify.app')
-    cy.contains('rev 1').should('exist')
+    cy.contains('rev 2').should('exist')
   })
 })
